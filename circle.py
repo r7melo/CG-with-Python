def716ac45f414cb58ed245183f12cc990d10bab
mestrome <romariomelo360@gmail.com>
2021-11-10 13:16:21 -0300

    circle

--- circle.py
+++ circle.py
@@ -21,47 +21,11 @@ class Circle:
         else:
             return sqrt(b)
 
-    def h(self, x):
-        a = abs(x)/2
-        b = 1-sqrt(a)
-        if b < 0:
-            b = 0
-        return -2.5*sqrt(b)
 
     def g(self, x,y, angle):
         return ( (y * cos((angle/180)*pi)) - (x * sin((angle/180)*pi)), (y * sin((angle/180)*pi)) + (x * cos((angle/180)*pi)))
 
 
-    def coracao(self):
-        dx = 0.1
-        glBegin(GL_POLYGON)
-        glColor3f(1,0,0)
-
-        x = -2
-        while -2 <= x <0:
-            glVertex3f(x, self.f(x), 0)
-            glVertex3f(x+dx, self.f(x+dx), 0)
-            x += dx
-        glEnd()
-
-        glBegin(GL_POLYGON)
-        x = 0
-        while 0 <= x <= 2:
-            glVertex3f(x, self.f(x), 0)
-            glVertex3f(x+dx, self.f(x+dx), 0)
-            x += dx
-        glEnd()
-
-        glBegin(GL_POLYGON)
-
-        x = -2
-        while -2 <= x < 2:
-            glVertex3f(x, self.h(x), 0)
-            glVertex3f(x+dx, self.h(x+dx), 0)
-            x += dx
-        glEnd()
-
-        
 
     def circle(self, angle_z=0):
         angle = 0
@@ -93,8 +57,8 @@ class Circle:
         
 
     def update(self):
-        
-        self.coracao()
+        self.esfera()
+        self.circle()
 
 
 if __name__=="__main__":
