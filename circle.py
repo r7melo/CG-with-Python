52f9badc1ea891401b9400a02874671587649438
mestrome <romariomelo360@gmail.com>
2021-11-10 14:53:16 -0300

    satelite

--- circle.py
+++ circle.py
@@ -9,56 +9,38 @@ from vector import Vector3d
 import pygame
 from math import *
 
+from cube import Cube
 
 class Circle:
+    def __init__(self):
+        self.rotation_x = 0
+        self.rotation_y = 0
 
-    # FUNÇÃO QUALQUER PARA SER REPRESENTADA
-    def f(self, x):
-        a = abs(x) - 1
-        b = 1 - pow(a, 2)
-        if b < 0:
-            return 0
-        else:
-            return sqrt(b)
-
-
-    def g(self, x,y, angle):
-        return ( (y * cos((angle/180)*pi)) - (x * sin((angle/180)*pi)), (y * sin((angle/180)*pi)) + (x * cos((angle/180)*pi)))
+    def spin(self, x,y, angle):
+        return ((y * cos((angle/180)*pi)) - (x * sin((angle/180)*pi)), (y * sin((angle/180)*pi)) + (x * cos((angle/180)*pi)))
 
 
 
-    def circle(self, angle_z=0):
-        angle = 0
-        dz = 10
-        rain = 5
-
-        glBegin(GL_LINES)
+    def circle(self, x=0, y=0, z=0, rain=0, angle_x=0, angle_y=0):
         glColor3f(0,1,0.7)
 
+        spiny = (0,0)
+        if angle_y > 0:
+            spiny = self.spin(0,rain,angle_y)
+
+        spinx = (0,0)
+        if angle_x > 0:
+            spinx = self.spin(0,rain,angle_x)
+
         
-        while 0 <= angle < 360:
-
-            glVertex3f(self.g(0,rain,angle)[0], self.g(0,rain,angle)[1], angle_z)
-            glVertex3f(self.g(0,rain,angle+dz)[0], self.g(0,rain,angle+dz)[1], angle_z)
-
-            angle += dz
-            
-        glEnd()
-
-    def esfera(self):
-        angle = 0
-        dy = 10
-
-        while 0 <= angle < 360:
-            self.circle(self.g(0,5,angle)[1])
-            angle += dy
+        Cube(spiny[0] ,spiny[1], spinx[0])
         
 
         
 
     def update(self):
-        self.esfera()
-        self.circle()
+        self.circle(angle_y=self.rotation_y, angle_x=self.rotation_x, rain=5)
+        self.rotation_y += 1
 
 
 if __name__=="__main__":
